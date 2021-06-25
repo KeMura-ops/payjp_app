ce0b2fc8fea1c33f3c2eb75b5be5e7eb23a540ad
KeMura-ops <kenti432@yahoo.co.jp>
2021-06-25 12:20:23 +0900

    マイページ編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def update
+    if current_user.update(user_params) # 更新できたかを条件分岐する
+      redirect_to root_path # 更新できたらroot_pathへ
+    else
+      redirect_to action: "show" # 失敗した場合マイページへ
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email) # 編集できる情報を制限
+  end
+
 end
