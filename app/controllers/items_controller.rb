fb99b6f1378917126e2936600093e6c7df9b6cff
KeMura-ops <kenti432@yahoo.co.jp>
2021-07-08 09:57:44 +0900

    オーダーアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,17 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: :order # 「find_item」を動かすアクションを限定
+
   def index
     @items = Item.all
   end
+
+  def order # 購入するときのアクションを定義
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
 end
